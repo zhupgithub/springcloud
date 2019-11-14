fdc5488e5c9191260c8d10e6dc46d00c6fab5278
zhupgithub <1539543015@qq.com>
2019-11-14 21:53:25 +0800

    测试config配置中心

--- main-mobile/src/main/java/com/zhupeng/controller/UserController.java
+++ main-mobile/src/main/java/com/zhupeng/controller/UserController.java
@@ -3,13 +3,19 @@ package com.zhupeng.controller;
 import com.zhupeng.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    @Value("${username}")
+    String username;
 
     @Autowired
     RedisUtil redisUtil;
@@ -18,6 +24,6 @@ public class UserController {
     public String test(){
         redisUtil.hset("zhupeng" , "test" , "zhp");
 
-        return "success";
+        return "success" + username;
     }
 }
