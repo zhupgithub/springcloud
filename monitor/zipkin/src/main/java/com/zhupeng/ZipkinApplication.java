3ee0495f68b18fb84eec6814fa7ec6d5bb2e47d8
zhupgithub <1539543015@qq.com>
2019-11-29 17:47:30 +0800

    aa

--- monitor/zipkin/src/main/java/com/zhupeng/ZipkinApplication.java
+++ monitor/zipkin/src/main/java/com/zhupeng/ZipkinApplication.java
@@ -3,6 +3,7 @@ package com.zhupeng;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,7 @@ import javax.sql.DataSource;
 
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableZipkinServer
 public class ZipkinApplication {
@@ -24,14 +25,9 @@ public class ZipkinApplication {
         SpringApplication.run(ZipkinApplication.class , args);
         log.info("zipkin监控启动完成！");
     }
-
 //    @Bean
+//    @Primary
 //    public MySQLStorage mySQLStorage(DataSource datasource) {
 //        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
 //    }
-    @Bean
-    @Primary
-    public MySQLStorage mySQLStorage(DataSource datasource) {
-        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
-    }
 }
